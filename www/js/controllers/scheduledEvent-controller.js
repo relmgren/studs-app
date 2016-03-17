1a60d976bfb9d76c5b7b03b09182e0b7fcdaaa33
Rasmus Elmgren <elmgren93@gmail.com>
2016-03-17 22:46:44 +0100

    an evenet now shows its title and position on a map

--- www/js/controllers/scheduledEvent-controller.js
+++ www/js/controllers/scheduledEvent-controller.js
@@ -1,4 +1,31 @@
-controllers.controller('scheduledEventCtrl', function($scope, happening) {
+controllers.controller('scheduledEventCtrl', function($scope, $state, $cordovaGeolocation, happening) {
     $scope.happening = happening;
+    var coordinates = {lat: $scope.happening.lat, lng: $scope.happening.lng};
+
+    var options = {timeout: 10000, enableHighAccuracy: false};
+
+    $cordovaGeolocation.getCurrentPosition(options). then(function(position) {
+        var latLng = new google.maps.LatLng(coordinates.lat, coordinates.lng);
+
+        var mapOptions = {
+            center: latLng,
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+          var marker = new google.maps.Marker({
+              map: $scope.map,
+              animation: google.maps.Animation.DROP,
+              position: latLng
+          });
+        });
+    }, function(err) {
+        console.err(err);
+    });
+
+
 
 });
