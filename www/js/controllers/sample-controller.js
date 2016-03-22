f56948e770638048ad1e04b5a7f354170d042e11
Rasmus Elmgren <elmgren93@gmail.com>
2016-03-22 14:56:01 +0100

    pushing angular-resource as it isnt installed correctly

--- www/js/controllers/sample-controller.js
+++ www/js/controllers/sample-controller.js
@@ -35,17 +35,3 @@ controllers.controller('sampleCtrl', ['$scope', 'Schedule', function($scope, Sch
 
 	$scope.init();
 }]);
-
-/*
-*	Second controller (should be in separate file)
-*/
-controllers.controller('challengeCtrl', ['$scope', 'appSettings', function($scope, appSettings) {
-	$scope.variable = "Second view!";
-	$scope.settings = appSettings;
-
-	$scope.init = function() {
-		// Run initial code here!
-	};
-
-	$scope.init();
-}]);
