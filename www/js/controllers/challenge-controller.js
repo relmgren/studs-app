62666d2aac1833c290ddeea00246a0433a407d69
aroshine <aroshine@kth.se>
2016-04-06 14:04:51 +0200

    challenge-controller fix

--- www/js/controllers/challenge-controller.js
+++ www/js/controllers/challenge-controller.js
@@ -1,13 +1,27 @@
 /*
 *	Second controller (should be in separate file)
 */
-controllers.controller('challengeCtrl', ['$scope', 'appSettings', function($scope, appSettings) {
+controllers.controller('challengeCtrl', ['$scope', 'Challenge',  function($scope, Schedule) {
 	$scope.status = "Here we are going to list some challenges for you to do in the States"
-	$scope.settings = appSettings;
 
-	$scope.init = function() {
-		// Run initial code here!
-	};
+	$scope.collection = Challenge.all();
+	$scope.previousEvents = Challenge.previous();
+	$scope.comingEvents = Challenge.coming();
+	$scope.groups = [$scope.previousEvents, $scope.comingEvents];
+	$scope.showing = false;
+	$scope.showPrevious = "Show previous events";
+	$scope.hidePrevious = "Hide previous events";
+	$scope.buttonText = $scope.showPrevious;
 
-	$scope.init();
+
+	$scope.toggle = function(){
+		$scope.showing = !$scope.showing;
+		if($scope.showing){
+				$scope.buttonText = $scope.hidePrevious;
+		} else {
+			$scope.buttonText = $scope.showPrevious;
+
+		}
+	}
+
 }]);
