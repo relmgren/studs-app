de8dd990030a63f3085d9669772e2679e4a1dc32
aroshine <aroshine@kth.se>
2016-04-06 13:58:29 +0200

    challenge-service

---
+++ www/js/services/challenge-service.js
@@ -0,0 +1,115 @@
+/**
+* Service that handles the schedule.
+**/
+
+services.factory('Challenge', function($resource) {
+    var challenge = [];
+    var api_key = "ZZKN2zrVZD7CJSRt9VLYit8XW4wqLP4E";
+    var comingEvents = [];
+    var previousEvents = [];
+
+    // Try LocalStorage
+    if(typeof(Storage) !== "undefined") {
+        // Woho! It worked. We must only store strings. Solution, stringify the js-objects when settings them!
+        challenge = JSON.parse(localStorage.getItem("challenge"));
+        if(challenge === null) {
+            // If nothing is yet set (first time use), use "default values"
+            challenge = [];
+        }
+    }
+
+    var challengeFactory = this.Challenge = $resource("https://api.mlab.com/api/1/databases/studs-app/collections/challenge/:id", {
+        apiKey : api_key
+    });
+
+
+
+
+
+    var divideEvents = function() {
+      comingEvents = [];
+      previousEvents = [];
+      var date = new Date(Date.now());
+      var month = date.getUTCMonth();
+      var day = date.getUTCDate();
+      for(i = 0; i < challenge.length; i++){
+        var date2 = new Date(challenge[i].date);
+
+        if(date2.getUTCMonth() < month){
+          previousEvents.push(challenge[i]);
+          continue;
+        }
+        if(date2.getUTCDate() < day && date2.getUTCMonth() == month){
+          previousEvents.push(challenge[i]);
+          continue;
+
+        }
+          comingEvents.push(challenge[i]);
+
+      }
+      return [previousEvents, comingEvents];
+
+    };
+    var sortCollectionByDate = function(){
+      challenge.sort(function compare(a,b) {
+        if (a.date < b.date)
+          return -1;
+        if (a.date > b.date)
+          return 1;
+        return 0;
+      });
+    }
+
+
+    var successCallback = function() {
+       challenge = results;
+        sortCollectionByDate();
+        divideEvents();
+        localStorage.removeItem("challenge");
+        localStorage.setItem("challenge", JSON.stringify(challenge));
+    };
+
+    var results = challengeFactory.query({}, successCallback, function(err){
+        console.log(err);
+
+        $ionicPopup.confirm({
+            title: "No connection (or server problem)",
+            content: "You are using a cached version of the challenge."
+        });
+    });
+
+
+
+    return {
+        all: function() {
+          
+            return challenge;
+        },
+        add: function(item) {
+           challenge.unshift(item);
+            // Save locally!
+            localStorage.setItem("challenge", JSON.stringify(challenge));
+        },
+        index: function(index) {
+            return challenge[index];
+        },
+        previous: function() {
+            var list = divideEvents();
+            return  list[0];
+        },
+        coming: function() {
+          var list = divideEvents();
+
+            return list[1];
+        },
+        remove: function(item) {
+            var index = challenge.indexOf(item);
+            if(index > -1) {
+                challenge.splice(index, 1);
+            }
+            // Save locally!
+            localStorage.setItem("challenge", JSON.stringify(challenge));
+        },
+        resource: this.Challenge
+    };
+});
