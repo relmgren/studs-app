05c155d035477ff09059e6110168f0b7c5e1b038
Rasmus Elmgren <elmgren93@gmail.com>
2016-04-20 10:44:48 +0200

    added camera functionality and the android platform

--- www/js/controllers/challengeInfo-controller.js
+++ www/js/controllers/challengeInfo-controller.js
@@ -6,22 +6,65 @@ controllers.controller('challengeInfoCtrl', function($scope, $state, specificCha
 	 "marcus", "emilio", "david", "victor", "nisse", "petriina", "rasmus", "rebecca", "sebastian", "fredrik", "per", "alexander", "william"];
 	$scope.specificChallenge = specificChallenge;
 	console.log(specificChallenge.img);
-	$scope.status = "Here we are going to list some challenges for you to do in the States"
+	$scope.status = "Here we are going to list some challenges for you to do in the States";
+
+	var pictureSource;
+	var destinationType;
+	var url;
+
+	// on deviceready check if camera is in use.
+	ionic.Platform.ready(function() {
+		if(!navigator.camera) {
+			//error
+			return;
+		}
+		pictureSource = navigator.camera.PictureSourceType.CAMERA;
+		destinationType = navigator.camera.DestinationType.FILE_URI;
+	});
+
+	$scope.selectedParticipants = [];
 
 	$scope.init = function() {
 		// Run initial code here!
 	};
 
 	$scope.imgClick = function(id){
-		console.log(id);
 		if(!document.getElementById(id).checked){
 			document.getElementById(""+id+"-img").style.border = "0";
+			$scope.selectedParticipants.splice($scope.selectedParticipants.indexOf(id) ,1);
+
 		} else {
 			document.getElementById(""+id+"-img").style.border = "2px solid #7ac89c";
+			$scope.selectedParticipants.push(id);
 		}
 	}
 
+	$scope.takePicture = function() {
+		var options = {
+			quality: 50,
+			destinationType: destinationType,
+			sourceType: pictureSource
+		};
+		if (!navigator.camera) {
+			//error
+			console.log("camera error before using camera");
+			return;
+		}
+		navigator.camera.getPicture(
+			//success callback
+			function (imageURI) {
+				$scope.mypicture = imageURI;
+			},
+			//error callback
+			function (err) {
+				console.log("camera error: ", err);
+			},
+			options);
+	};
+
 	$scope.newUpload = function(){
+		$scope.takePicture();
+
 		document.getElementById('imgUploader').style.display = "block";
 	}
 });
